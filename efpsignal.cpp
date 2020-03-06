68d1673baafa61090f8f19df60ba692065e9e46b
Anders Cedronius <anders.cedronius@icloud.com>
2020-03-06 08:46:12 +0100

    Created base project

---
+++ efpsignal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anders Cedronius on 2020-03-06.
+//
+
+#include "efpsignal.h"
---
+++ efpsignal.h
@@ -0,0 +1,12 @@
+//
+// Created by Anders Cedronius on 2020-03-06.
+//
+
+#ifndef EFPSIGNAL__EFPSIGNAL_H
+#define EFPSIGNAL__EFPSIGNAL_H
+
+class efpsignal {
+
+};
+
+#endif //EFPSIGNAL__EFPSIGNAL_H
